c276bb7536b30a19c01e1b92f4b349995f49483e
messiasGeovani <messias.geovani.lima@gmail.com>
2020-04-19 00:30:59 -0300

    feat(evaluateApp): evaluation methods and controllers created

---
+++ src/App/index.js
@@ -0,0 +1,26 @@
+//  App module
+const express = require('express')
+const app = express()
+
+// morgan config
+const morgan = require('morgan')
+// cors config
+const cors = require('cors')
+
+// routes
+const routes = require('./routes')
+
+// app config
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(routes)
+
+// 404 error config
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: 'Not found.'
+    })
+})
+
+module.exports = app
